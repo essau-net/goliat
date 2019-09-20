814d3009f4601b852a15167f603d961d3b65144e
hesau2001 <hesau2001@gmail.com>
2019-09-19 20:41:22 -0500

    Nada en especial

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 var navbar = {
     inicio = getElementById("#inicio"),
-    inicio = getElementById("#info"),
-    inicio = getElementById("#contacto")
+    informacion = getElementById("#info"),
+    contacto = getElementById("#contacto")
+}
+
+navbar.contacto.addEventListener("mousedown", contacto)
+
+function contacto() {
+    window.sr = ScrollReveal()
+
+    sr.reveal('.contacto', {
+        duration: 3000,
+        diastance: '300px',
+        origin: 'bottom'
+    })
+
 }
