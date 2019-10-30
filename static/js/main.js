1fb1a367de29c94fa9fa414444148bbfe26558a6
Esau Macias <hesau2001@gmail.com>
2019-10-30 09:48:11 -0600

    Cambio de registro y pagina perfil casi acabada

--- static/js/main.js
+++ static/js/main.js
@@ -1,38 +1,5 @@
 window.addEventListener("load",loadPagina)
 
 function loadPagina(){
-    var asideMenu = {
-        home: document.getElementById("homeM"),
-        perfil: document.getElementById("perfilM"),
-        actividades: document.getElementById("actividadesM")
-    }
-    asideMenu.home.addEventListener("click", cambioMenu)
+    var activarMovimiento = document.getElementById(    )
 }
-function cambioMenu (event){
-    console.log(event)
-}
-function probando(){
-    alert("Fuck yeaH it works")
-}
-
-/*;
-var home = document.getElementById("homeM")
-
-console.log(asideMenu.home)
-console.log(home)
-//asideMenu.home.addEventListener("click", probando)
-
-
-
-asideMenu.home.addEventListener("click", cambioPagina(asideMenu.home))
- var cambioPagina = function (eleccion){
-     return function cambioColor(event){
-         if(asideMenu.home == eleccion){
-
-         }else
-        asideMenu.home.classList.remove("active")
-        asideMenu.perfil.classList.remove("active")
-        asideMenu.perfil.classList.remove("active")
-        eleccion.classList.add("active")
-     }
- }*/
