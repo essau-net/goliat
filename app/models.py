8ed8db5731471c6fcaca6fa10784defd28f40664
Hector Esau Macias Javier <hesau2001@hesau2001.gateway.huawei.net>
2020-03-31 10:26:11 -0600

    Agregando blue print y otras cosas aprendidas en platzi

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from flask_login import UserMixin
+
+from mysql_service import *
+
+
+class UserData:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+class UserModel(UserMixin):
+    def __init__(self, user_data):
+        self.id = user_data.username
+        self.password = user_data.password
+
+    @staticmethod
+    def query(user_id)
+        pass
